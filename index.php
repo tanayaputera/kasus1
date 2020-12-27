54d3c8302735203d75e0de1ce55ababe73eb3cb2
tanayaputera <bimaputeraa@gmail.com>
2020-12-27 15:33:26 +0700

    mengubah file indwex.php

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     <title>kasus1</title>
 </head>
 <body>
-    hello world
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi voluptatem repellendus laboriosam aspernatur voluptates quis ipsam sed ipsum, magnam inventore, veritatis quia voluptate autem rem ducimus delectus illo velit.
 </body>
 </html>
